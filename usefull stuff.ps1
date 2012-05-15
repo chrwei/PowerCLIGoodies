b44744466675582695f4bd4859124df74634a6af
Chris Weiss <cweiss@gmail.com>
2012-05-15 08:24:37 -0500

    Update usefull stuff.ps1

--- usefull stuff.ps1
+++ usefull stuff.ps1
@@ -6,6 +6,8 @@ Connect-VIServer 'vcenter'
 
 #disconnect all cdroms
 #Get-VM | Where-Object {$_ | Get-CDDrive | Where-Object { $_.ConnectionState.Connected -eq "true"  } } | Get-CDDrive | Set-CDDrive -Connected $false -Confirm:$false
+#unmap all cdroms
+#Get-VM | Where-Object {$_ | Get-CDDrive | Where-Object { $_.ConnectionState.Connected -eq "true"  } } | Get-CDDrive | Set-CDDrive -NoMedia -Connected $false -StartConnected $false 
 
 #connect all nics
 #Get-VM | Where-Object {$_ | Get-NetworkAdapter | Where-Object { $_.ConnectionState.Connected -eq "false"  } } | Get-NetworkAdapter | Set-NetworkAdapter  -Connected $false -Confirm:$false
@@ -13,3 +15,13 @@ Connect-VIServer 'vcenter'
 #get a vm's vmx path
 #Get-VM "xkerio 1" | Select Name, @{N="VMX";E={$_.Extensiondata.Summary.Config.VmPathName}}
 
+#set a vmx option on all guests
+#$Spec = new-object VMware.Vim.VirtualMachineConfigSpec
+#$Spec.extraconfig += New-Object VMware.Vim.optionvalue
+#$Spec.extraconfig[0].Key= "keyboard.typematicMinDelay"
+#$Spec.extraconfig[0].Value= "2000000"
+#Get-VM  | % {
+#    $_.Extensiondata.ReconfigVM($Spec)
+#}
+#select the option
+#Get-VM | select Name, @{N="MinDelay";E={($_.Extensiondata.Config.ExtraConfig | where {$_.Key -eq "keyboard.typematicMinDelay"}).Value}}
