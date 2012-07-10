024103a3c98aa8aa4fa1c246d834a215fc05ea2f
Chris Weiss <cweiss@gmail.com>
2012-07-10 14:07:10 -0500

    Update master

--- usefull stuff.ps1
+++ usefull stuff.ps1
@@ -25,3 +25,6 @@ Connect-VIServer 'vcenter'
 #}
 #select the option
 #Get-VM | select Name, @{N="MinDelay";E={($_.Extensiondata.Config.ExtraConfig | where {$_.Key -eq "keyboard.typematicMinDelay"}).Value}}
+
+#1 hour 20 second ave cpu usage for all running vm
+#Get-VM | Where-Object {$_.PowerState -eq "PoweredOn"} | select Name, @{N="1Hour CpuAve";E={[System.Math]::Round((Get-Stat -Entity $_ -Stat cpu.usage.average -Realtime | Select -ExpandProperty Value | Measure-Object -Average -Maximum).Average, 2)}}
